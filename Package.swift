9b0fed2a26a7cd5bbbc1295f59697ed33889ae57
Zach Eriksen <zeriksen@fnoex.com>
2020-10-07 15:30:46 -0500

    Update to Swift 5.1

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
-// swift-tools-version:4.2
-// Managed by ice
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
